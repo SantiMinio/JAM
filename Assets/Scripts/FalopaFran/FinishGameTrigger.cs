38c1c90ce8a270a7dd6fa609afdf1fe5f46d4e18
SantiMinio <santiago.mino@davinci.edu.ar>
2021-06-13 07:16:27 -0300

    AFINSADVF KIASBG

--- Assets/Scripts/FalopaFran/FinishGameTrigger.cs
+++ Assets/Scripts/FalopaFran/FinishGameTrigger.cs
@@ -8,6 +8,13 @@ public class FinishGameTrigger : MonoBehaviour
     [SerializeField] private LayerMask triggerLayers;
 
     [SerializeField] private float feedbackTime;
+    Grayscale_Post_Process pp;
+
+    private void Start()
+    {
+        pp = FindObjectOfType<Grayscale_Post_Process>();
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         if ((triggerLayers.value & (1 << other.gameObject.layer)) > 0)
@@ -15,13 +22,22 @@ public class FinishGameTrigger : MonoBehaviour
             Debug.Log("entro");
             Main.instance.eventManager.TriggerEvent(GameEvents.HotelArrive);
             StartCoroutine(WaitToFeedback());
+            UIManager.instance.gameObject.gameObject.SetActive(false);
         }
     }
 
     IEnumerator WaitToFeedback()
     {
-        yield return new WaitForSeconds(feedbackTime);
+        float timer = 0;
+        while (timer < feedbackTime)
+        {
+            timer += Time.deltaTime;
+            pp.heartEffect = timer / feedbackTime;
+            yield return new WaitForEndOfFrame();
+        }
 
+        Destroy(SaveData.saveData.gameObject);
         SceneManager.LoadScene("FranMapRemake1 1");
+        
     }
 }
