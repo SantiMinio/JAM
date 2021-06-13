dfa6a577cfd7e3567fc2438a69d70d88810fcad7
Dario-Carbonell <guillermo.carbonellveracruz@davinci.edu.ar>
2021-06-13 06:20:02 -0300

    Le baje el volumne a la musca y al Falling

--- Assets/Gente_Callendo_Sonidos.cs
+++ Assets/Gente_Callendo_Sonidos.cs
@@ -4,17 +4,23 @@ using UnityEngine;
 
 public class Gente_Callendo_Sonidos : MonoBehaviour
 {
-    public AudioSource[] audioSources;
+    public AudioSource audioSource;
+    public AudioSource audioSource2;
 
     private void Awake()
     {        
-        audioSources = GetComponentsInChildren<AudioSource>();
+        audioSource = GetComponent<AudioSource>();
+        audioSource2 = GetComponent<AudioSource>();
     }
     private void OnParticleCollision(GameObject other)
     {
-        foreach (AudioSource sonidos in audioSources)
-        {
-            sonidos.Play();
-        }        
-    }    
+        audioSource.Play();
+        audioSource2.Play();              
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        audioSource.Play();
+        audioSource2.Play();
+    }
 }
