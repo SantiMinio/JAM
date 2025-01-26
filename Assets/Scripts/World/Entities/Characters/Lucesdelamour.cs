479f5b846f8a99bdaa039a2c143d0c84812c2a8f
Gonzalou <gonzagtf12@gmail.com>
2025-01-26 16:55:40 -0300

    un poco de dressing,correccion de lasers y enemigos

--- Assets/Scripts/World/Entities/Characters/Lucesdelamour.cs
+++ Assets/Scripts/World/Entities/Characters/Lucesdelamour.cs
@@ -5,9 +5,7 @@ using UnityEngine;
 public class Lucesdelamour : MonoBehaviour
 {  
     
-    public Light luzPibe;
-    public Light luzPiba;
-    public Light LoveLight;
+    
     public float loveRange;
     public float loveMaxRange;
     public float IntensityMultipler;
@@ -15,32 +13,13 @@ public class Lucesdelamour : MonoBehaviour
     public float gppmultipler;
     public DistanceController dc;
     private void Awake()
-    {
-        LoveLight.gameObject.SetActive(false);
+    {      
         loveMaxRange = dc.GetMaxDistanceBetweenCharacters();
         loveRange = dc.GetDistanceBetweenCharacters();       
     }
 
     private void Update()
-    {
-        luzPiba.transform.position = Main.instance.GetWife().transform.position + Vector3.up ;
-        luzPibe.transform.position = Main.instance.GetHusband().transform.position + Vector3.up;
-        luzPiba.intensity = -dc.GetDistanceBetweenCharacters() + IntensityMultipler + loveMaxRange;
-        luzPibe.intensity = -dc.GetDistanceBetweenCharacters() + IntensityMultipler + loveMaxRange;
-       
-        if (dc.GetDistanceBetweenCharacters() < 2)
-        {
-            luzPiba.gameObject.SetActive(false);
-            luzPibe.gameObject.SetActive(false);
-            LoveLight.gameObject.SetActive(true);
-            LoveLight.intensity= -dc.GetDistanceBetweenCharacters() + IntensityMultipler2 + loveMaxRange;
-        }
-        else
-        {
-            luzPiba.gameObject.SetActive(true);
-            luzPibe.gameObject.SetActive(true);
-            LoveLight.gameObject.SetActive(false);
-        }
-        LoveLight.transform.position = Vector3.Lerp(luzPiba.transform.position, luzPibe.transform.position, 0.5f);
+    {             
+        
     }
 }
